b95dd89d192515b4bf7b24e3187fd9692c81bcd7
Nicolas <nicolas.gracioli@gmail.com>
2025-04-10 21:44:52 -0300

    feat: add JsonFiles

---
+++ app/src/main/java/sunshare/json_manager/JsonsFiles.java
@@ -0,0 +1,6 @@
+package sunshare.json_manager;
+
+public class JsonsFiles {
+    static private final String path = System.getProperty("user.dir");
+    static final public String users = path + "/database/users.json";
+}
