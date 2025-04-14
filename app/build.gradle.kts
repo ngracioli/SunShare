0bf70e85204c2318e3c3baa347c01a80b104f8b4
peguinm <pedroozorio30@gmail.com>
2025-04-13 23:47:46 -0300

    feat(build): update jackson versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 
 dependencies {
     // Jackson
-    implementation("com.fasterxml.jackson.core:jackson-annotations:3.0-rc2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
     implementation("com.fasterxml.jackson.core:jackson-core:2.18.3")
 
     // Jansi
