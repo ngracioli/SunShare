55afe5da6b0505224d0bee6e13e5c5f1b2d24579
Nicolas <nicolas.gracioli@gmail.com>
2025-05-01 17:54:36 -0300

    refactor: improve input validation in EntryMenu

--- app/src/main/java/sunshare/console/menus/EntryMenu.java
+++ app/src/main/java/sunshare/console/menus/EntryMenu.java
@@ -7,32 +7,40 @@ import sunshare.console.menus.authentication.LoginMenu;
 import sunshare.console.menus.authentication.RegisterMenu;
 
 public class EntryMenu {
+    private static boolean exit = false;
+
     public static void show(Scanner scanner, AuthService authService) {
+        while (!exit) {
+            showMenu(scanner, authService);
+        }
+    }
+
+    public static void showMenu(Scanner scanner, AuthService authService) {
         ConsoleUtils.clearConsole();
         ConsoleUtils.printTitle("Bem-vindo ao SunShare");
+        ConsoleUtils.printExit("0. Sair");
         ConsoleUtils.printOption("1. Cadastrar");
         ConsoleUtils.printOption("2. Entrar");
-        ConsoleUtils.printExit("3. Sair");
 
         System.out.print("Escolha uma opção: ");
         int option = scanner.nextInt();
         scanner.nextLine();
 
         switch (option) {
+            case 0:
+                ConsoleUtils.exit();
+                exit = true;
+                break;
             case 1:
                 RegisterMenu.show(scanner, authService);
                 break;
             case 2:
                 LoginMenu.show(scanner, authService);
                 break;
-            case 3:
-                ConsoleUtils.printExit("Saindo...");
-                ConsoleUtils.timerConsole(2000);
-                System.exit(0);
-                break;
             default:
-                System.out.println("Opção inválida. Tente novamente.");
+                ConsoleUtils.printError("Opção inválida. Tente novamente.");
                 ConsoleUtils.timerConsole(2000);
+                break;
         }
     }
 }
