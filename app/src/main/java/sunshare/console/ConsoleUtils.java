b68bfd240cf3ef3348f4e4fdd99caf8bf8891bb7
Nicolas <nicolas.gracioli@gmail.com>
2025-05-01 15:58:03 -0300

    feat: add ConsoleUtils class for colored console output and utilities

---
+++ app/src/main/java/sunshare/console/ConsoleUtils.java
@@ -0,0 +1,52 @@
+package sunshare.console;
+
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class ConsoleUtils {
+    static {
+        AnsiConsole.systemInstall();
+    }
+
+    public static void uninstall() {
+        AnsiConsole.systemUninstall();
+    }
+
+    public static void printTitle(String title) {
+        System.out.println(ansi().fgBrightYellow().bold().a(title).reset());
+    }
+
+    public static void printOption(String option) {
+        System.out.println(ansi().fgBrightCyan().a(option).reset());
+    }
+
+    public static void printError(String message) {
+        System.out.println(ansi().fgBrightRed().bold().a(message).reset());
+    }
+
+    public static void printExit(String message) {
+        printError(message);
+    }
+
+    public static void timerConsole(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Erro no timer");
+        }
+    }
+
+    public static void clearConsole() {
+        try {
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.contains("win")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao limpar o console");
+        }
+    }
+}
