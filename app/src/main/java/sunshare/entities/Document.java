57ec6c6a9acf2b3ac0181a3040dc000352f3f57a
Nicolas <nicolas.gracioli@gmail.com>
2025-05-01 18:07:50 -0300

    refactor: change documentType field type to DocumentTypes in Document entity

--- app/src/main/java/sunshare/entities/Document.java
+++ app/src/main/java/sunshare/entities/Document.java
@@ -4,23 +4,22 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Document {
-
-    private String documentType;
+    private DocumentTypes documentType;
     private String document;
 
     @JsonCreator
     public Document(
-            @JsonProperty("documentType") String documentType,
+            @JsonProperty("documentType") DocumentTypes documentType,
             @JsonProperty("document") String document) {
         this.documentType = documentType;
         this.document = document;
     }
 
-    public String getDocumentType() {
+    public DocumentTypes getDocumentType() {
         return documentType;
     }
 
-    public void setDocumentType(String documentType) {
+    public void setDocumentType(DocumentTypes documentType) {
         this.documentType = documentType;
     }
 
