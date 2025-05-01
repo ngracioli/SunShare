8c6a5552d7c6ecb4c832f145d549b7806dc2534e
Nicolas <nicolas.gracioli@gmail.com>
2025-05-01 18:28:11 -0300

    feat: add uuid field to Supplier constructor

--- app/src/main/java/sunshare/entities/Supplier.java
+++ app/src/main/java/sunshare/entities/Supplier.java
@@ -8,11 +8,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Supplier extends User {
     @JsonCreator
     public Supplier(
+            @JsonProperty("uuid") String uuid,
             @JsonProperty("name") String name,
             @JsonProperty("email") String email,
             @JsonProperty("password") String password,
             @JsonProperty("address") Address address,
             @JsonProperty("document") Document document) {
-        super(name, email, password, true, address, document);
+        super(uuid, name, email, password, true, address, document);
     };
 }
