86525ce396d31b0f7f09d57a79cffe3a1ba7431d
ngracioli <nicolas.gracioli@gmail.com>
2025-05-08 21:21:09 -0300

    chore: remove unused imports from App.java

--- app/src/main/java/sunshare/app/App.java
+++ app/src/main/java/sunshare/app/App.java
@@ -2,13 +2,8 @@ package sunshare.app;
 
 import java.util.Scanner;
 
-import sunshare.entities.address.Address;
-import sunshare.entities.document.Document;
-import sunshare.entities.user.User;
 import sunshare.services.AuthService;
-import sunshare.console.menus.*;
 import sunshare.console.menus.entry.EntryMenu;
-import sunshare.console.menus.utils.ConsoleUtils;
 
 public class App {
 
