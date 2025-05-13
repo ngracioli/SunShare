39e02cd9384b8373c69a355b2aa7b396a76e094e
ngracioli <nicolas.gracioli@gmail.com>
2025-05-13 19:48:10 -0300

    feat: add getAll, getOpen, create, and delete methods to OfertService

--- app/src/main/java/sunshare/services/OfertService.java
+++ app/src/main/java/sunshare/services/OfertService.java
@@ -1,5 +1,11 @@
 package sunshare.services;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import sunshare.entities.energy.Energy;
+import sunshare.entities.ofert.Ofert;
+import sunshare.entities.ofert.OfertStatus;
 import sunshare.json.manager.JsonManager;
 import sunshare.json.manager.JsonsFiles;
 
@@ -11,10 +17,24 @@ public class OfertService {
         jsonManager = new JsonManager(JsonsFiles.oferts);
     }
 
-    public void create() {
-
+    public ArrayList<Ofert> getAll() {
+        return jsonManager.select(Ofert.class, o -> true);
     }
 
+    public ArrayList<Ofert> getOpen() {
+        return jsonManager.select(Ofert.class, o -> true);
+    }
 
+    public void create(String supplierUuid, Energy energy) {
+        final Ofert ofert = new Ofert(UUID.randomUUID().toString(), supplierUuid, energy, OfertStatus.open);
+
+        jsonManager.insert(Ofert.class, jsonManager.toJsonNode(ofert));
+    }
+
+    public void delete(String uuid) {
+        jsonManager.delete(Ofert.class, o -> {
+            return o.getUuid().equals(uuid);
+        });
+    }
 
 }
