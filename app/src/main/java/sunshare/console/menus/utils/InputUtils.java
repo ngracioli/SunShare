9275542e0a7ba27d4b48c111ad199bb6a08b1766
ngracioli <nicolas.gracioli@gmail.com>
2025-05-15 20:52:48 -0300

    feat: add InputUtils with reusable readIntOption for menu input validation

---
+++ app/src/main/java/sunshare/console/menus/utils/InputUtils.java
@@ -0,0 +1,27 @@
+package sunshare.console.menus.utils;
+
+import java.util.Scanner;
+
+public class InputUtils {
+    public static int readIntOption(Scanner scanner, String prompt, String errorMsg, int... validOptions) {
+        int value;
+        boolean valid;
+        do {
+            System.out.println(prompt);
+            while (!scanner.hasNextInt()) {
+                System.out.println(errorMsg);
+                scanner.next();
+            }
+            value = scanner.nextInt();
+            valid = false;
+            for (int v : validOptions) {
+                if (value == v)
+                    valid = true;
+            }
+            if (!valid)
+                System.out.println(errorMsg);
+        } while (!valid);
+        scanner.nextLine();
+        return value;
+    }
+}
