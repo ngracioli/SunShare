de2a68f05f8a8e8b62f43be40d92f9163483c2e2
ngracioli <nicolas.gracioli@gmail.com>
2025-05-15 20:05:40 -0300

    feat: create Offer class in offer package

---
+++ app/src/main/java/sunshare/entities/offer/Offer.java
@@ -0,0 +1,58 @@
+package sunshare.entities.offer;
+
+import sunshare.entities.energy.Energy;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Offer {
+    private String uuid;
+    private String supplierUuid;
+    private Energy energy;
+    private OfferStatus status;
+
+    @JsonCreator
+    public Offer(
+            @JsonProperty("uuid") String uuid,
+            @JsonProperty("supplier_uuid") String supplierUuid,
+            @JsonProperty("energy") Energy energy,
+            @JsonProperty("status") OfferStatus status) {
+        this.uuid = uuid;
+        this.supplierUuid = supplierUuid;
+        this.energy = energy;
+        this.status = status;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getSupplierUuid() {
+        return supplierUuid;
+    }
+
+    public void setSupplierUuid(String supplierUuid) {
+        this.supplierUuid = supplierUuid;
+    }
+
+    public Energy getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(Energy energy) {
+        this.energy = energy;
+    }
+
+    public OfferStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OfferStatus status) {
+        this.status = status;
+    }
+
+}
