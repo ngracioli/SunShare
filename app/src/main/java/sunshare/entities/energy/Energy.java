30d681d773d66c6a960bde598f643895a6d29063
ngracioli <nicolas.gracioli@gmail.com>
2025-05-15 20:04:04 -0300

    feat: add quantity field to Energy class

--- app/src/main/java/sunshare/entities/energy/Energy.java
+++ app/src/main/java/sunshare/entities/energy/Energy.java
@@ -6,12 +6,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Energy {
     private String symbol;
     private double amount;
+    private double quantity;
 
     @JsonCreator
-    Energy(
+    public Energy(
             @JsonProperty("symbol") String symbol,
+            @JsonProperty("quantity") double quantity,
             @JsonProperty("amount") double amount) {
         this.amount = amount;
+        this.quantity = quantity;
         this.symbol = symbol;
     }
 
@@ -30,4 +33,13 @@ public class Energy {
     public double getAmount() {
         return amount;
     }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
 }
