d29b69c0df82e29e31ffe896d6c4a308ad36edf4
ngracioli <nicolas.gracioli@gmail.com>
2025-05-15 21:10:41 -0300

    feat: add proposals file path to JsonsFiles

--- app/src/main/java/sunshare/json/manager/JsonsFiles.java
+++ app/src/main/java/sunshare/json/manager/JsonsFiles.java
@@ -3,5 +3,6 @@ package sunshare.json.manager;
 public class JsonsFiles {
     static private final String path = System.getProperty("user.dir");
     static final public String users = path + "/database/users.json";
-    static final public String oferts = path + "/database/oferts.json";
+    static final public String offers = path + "/database/offers.json";
+    static final public String proposals = path + "/database/proposals.json";
 }
