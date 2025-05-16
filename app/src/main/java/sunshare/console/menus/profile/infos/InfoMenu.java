5db71f8464faf273c262cc758f310d13c5a97e97
ngracioli <nicolas.gracioli@gmail.com>
2025-05-15 22:03:30 -0300

    feat: create InfoMenu to display user information

--- app/src/main/java/sunshare/console/menus/profile/infos/InfoMenu.java
+++ app/src/main/java/sunshare/console/menus/profile/infos/InfoMenu.java
@@ -3,12 +3,24 @@ package sunshare.console.menus.profile.infos;
 import java.util.Scanner;
 
 import sunshare.console.menus.utils.ConsoleUtils;
+import sunshare.entities.address.Address;
+import sunshare.entities.document.Document;
 import sunshare.entities.user.User;
 
 public class InfoMenu {
 
-    public void showMenu(Scanner scanner, User user) {
-        // bla bla bla bla informações
+    public InfoMenu(Scanner scanner, User user) {
+        final Address address = user.getAddress();
+        final Document document = user.getDocument();
+        ConsoleUtils.clearConsole();
+        ConsoleUtils.printTitle("Minhas informações");
+        ConsoleUtils.printSuccess("Nome: " + user.getName());
+        ConsoleUtils.printSuccess("Email: " + user.getEmail());
+        ConsoleUtils.printSuccess("Tipo: " + (user.isSupplier() ? "Fornecedor" : "Consumidor"));
+        ConsoleUtils.printSuccess("Endereço: " + address.getStreet() + ", " + address.getNeighborhood() + ", "
+                + address.getCity() + ", " + address.getState() + ", " + address.getCep());
+        ConsoleUtils.printSuccess("Tipo de documento: " + document.getDocumentType().type);
+        ConsoleUtils.printSuccess("Número do documento: " + document.getDocument());
         ConsoleUtils.printError("Pressione ENTER para voltar");
         scanner.nextLine();
         return;
