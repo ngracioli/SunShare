55b2fedad56dca674d253575329a76187039dc96
peguinm <pedroozorio30@gmail.com>
2025-05-17 17:37:22 -0300

    refactor: rename JsonsFiles to JsonFiles

--- app/src/main/java/sunshare/json/manager/JsonFiles.java
+++ app/src/main/java/sunshare/json/manager/JsonFiles.java
@@ -1,6 +1,6 @@
 package sunshare.json.manager;
 
-public class JsonsFiles {
+public class JsonFiles {
     static private final String path = System.getProperty("user.dir");
     static final public String users = path + "/database/users.json";
     static final public String offers = path + "/database/offers.json";
