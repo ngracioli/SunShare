87c65acbc0bd925f3c93e83325a51429112e0800
peguinm <pedroozorio30@gmail.com>
2025-05-17 22:12:21 -0300

    refactor: remove unused import from App.java

--- app/src/main/java/sunshare/app/App.java
+++ app/src/main/java/sunshare/app/App.java
@@ -2,15 +2,14 @@ package sunshare.app;
 
 import sunshare.console.menus.entry.EntryMenu;
 import sunshare.services.AuthService;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-       final AuthService authService = new AuthService();
-       final Scanner scanner = new Scanner(System.in);
+        final AuthService authService = new AuthService();
+        final Scanner scanner = new Scanner(System.in);
 
-       new EntryMenu(scanner, authService);
+        new EntryMenu(scanner, authService);
     }
 }
