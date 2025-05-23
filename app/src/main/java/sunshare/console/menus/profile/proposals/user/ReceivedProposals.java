6313cc13fcf9c4464d423cb79502cf038c3bc8db
ngracioli <nicolas.gracioli@gmail.com>
2025-05-23 00:12:35 -0300

    feat: create ReceivedProposals class for supplier to manage received proposals

---
+++ app/src/main/java/sunshare/console/menus/profile/proposals/user/ReceivedProposals.java
@@ -0,0 +1,95 @@
+package sunshare.console.menus.profile.proposals.user;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import sunshare.console.menus.utils.ConsoleUtils;
+import sunshare.console.menus.utils.InputUtils;
+import sunshare.entities.offer.Offer;
+import sunshare.entities.proposal.Proposal;
+import sunshare.entities.proposal.ProposalStatus;
+import sunshare.entities.user.User;
+import sunshare.services.OfferService;
+import sunshare.services.ProposalService;
+import sunshare.services.UserService;
+
+public class ReceivedProposals {
+    private boolean exit = false;
+
+    public ReceivedProposals(Scanner scanner, User user) {
+        while (!exit) {
+            showMenu(scanner, user);
+        }
+    }
+
+    public void showMenu(Scanner scanner, User user) {
+        ProposalService proposalService = new ProposalService();
+        OfferService offerService = new OfferService();
+        UserService userService = new UserService();
+        ArrayList<Proposal> proposals = proposalService.getAllBySupplierUuid(user.getUuid());
+
+        ArrayList<Proposal> pendingProposals = new ArrayList<>();
+        for (Proposal proposal : proposals) {
+            if (proposal.getStatus() == ProposalStatus.open) {
+                pendingProposals.add(proposal);
+            }
+        }
+
+        ConsoleUtils.clearConsole();
+        ConsoleUtils.printTitle("-- Propostas Recebidas --");
+        ConsoleUtils.printExit("0. Voltar\n");
+        if (pendingProposals.isEmpty()) {
+            ConsoleUtils.printError("Nenhuma proposta encontrada.");
+        } else {
+            for (int i = 0; i < pendingProposals.size(); i++) {
+                Proposal proposal = pendingProposals.get(i);
+                Offer offer = offerService.getByUuid(proposal.getOfferUuid());
+                User buyer = userService.getByUuid(proposal.getBuyerUuid());
+                ConsoleUtils.printSuccess(String.format("%d. Proposta", i + 1));
+                String proposalInfo = String.format(
+                        "Comprador: %s\n" +
+                                "Produto: %s %s\n" +
+                                "Valor pedido: R$ %.2f\n" +
+                                "Valor oferecido: R$ %.2f\n",
+                        buyer.getName(),
+                        offer.getEnergy().getQuantity(),
+                        offer.getEnergy().getSymbol(),
+                        offer.getEnergy().getAmount(),
+                        proposal.getProposalValue());
+                ConsoleUtils.printOption(proposalInfo);
+            }
+
+        }
+        int option = InputUtils.readIntOption(scanner, "Escolha uma opção: ", "Digite 0 ou o número de uma proposta.",
+                InputUtils.range(0, pendingProposals.size()));
+        switch (option) {
+            case 0:
+                exit = true;
+                break;
+            default:
+                if (option > 0 && option <= pendingProposals.size()) {
+                    var proposal = pendingProposals.get(option - 1);
+                    ConsoleUtils.printSuccess("Proposta selecionada!");
+                    ConsoleUtils.printOption("Deseja aceitar ou recusar a proposta? (aceitar/recusar)");
+                    String response = scanner.nextLine();
+                    if (response.equalsIgnoreCase("aceitar")) {
+                        ConsoleUtils.printSuccess("Tem certeza que deseja aceitar a proposta? (s/n)");
+                        String confirm = scanner.nextLine();
+                        if (!confirm.equalsIgnoreCase("s")) {
+                            ConsoleUtils.printError("Operação cancelada.");
+                            return;
+                        }
+                        proposalService.accept(proposal.getProposalUuid());
+                        ConsoleUtils.printSuccess("Proposta aceita com sucesso!");
+                    } else if (response.equalsIgnoreCase("recusar")) {
+                        ConsoleUtils.printError("Proposta rejeitada com sucesso!");
+                    } else {
+                        ConsoleUtils.printError("Opção inválida.");
+                    }
+                }
+                break;
+        }
+    }
+}
