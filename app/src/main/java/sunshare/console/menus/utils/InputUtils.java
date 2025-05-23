4c5d794ee5ee6929d9e040f2d84033082e4d6b82
ngracioli <nicolas.gracioli@gmail.com>
2025-05-23 00:07:14 -0300

    feat: add range utility method to InputUtils for option validation

--- app/src/main/java/sunshare/console/menus/utils/InputUtils.java
+++ app/src/main/java/sunshare/console/menus/utils/InputUtils.java
@@ -24,4 +24,12 @@ public class InputUtils {
         scanner.nextLine();
         return value;
     }
+
+    public static int[] range(int start, int end) {
+        int[] arr = new int[end - start + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = start + i;
+        }
+        return arr;
+    }
 }
