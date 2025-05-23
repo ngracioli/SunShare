5236cf6db9bb5c87a0ca6f30b51bdcd3b430161f
ngracioli <nicolas.gracioli@gmail.com>
2025-05-23 00:10:59 -0300

    feat: implement ProposalService with proposal creation, status management, and queries

--- app/src/main/java/sunshare/services/ProposalService.java
+++ app/src/main/java/sunshare/services/ProposalService.java
@@ -3,24 +3,86 @@ package sunshare.services;
 import java.util.ArrayList;
 import java.util.UUID;
 
+import sunshare.entities.offer.Offer;
+import sunshare.entities.offer.OfferStatus;
 import sunshare.entities.proposal.Proposal;
+import sunshare.entities.proposal.ProposalStatus;
 import sunshare.json.manager.JsonFiles;
 import sunshare.json.manager.JsonManager;
 
-public class ProposalService {
-    private final JsonManager jsonManager;
+public class ProposalService extends BaseService {
+    private final JsonManager offerJsonManager;
 
     public ProposalService() {
-        jsonManager = new JsonManager(JsonFiles.proposals);
+        super(JsonFiles.proposals);
+        offerJsonManager = new JsonManager(JsonFiles.offers);
     }
 
-    public Proposal create(String buyerUuid, String supplierUuid, String offerUuid) {
-        final Proposal proposal = new Proposal(UUID.randomUUID().toString(), buyerUuid, supplierUuid, offerUuid);
+    public Proposal create(String buyerUuid, String supplierUuid, String offerUuid, double proposalValue) {
+        final Proposal proposal = new Proposal(generateUuid(), buyerUuid, supplierUuid, offerUuid, proposalValue,
+                ProposalStatus.open);
 
         return jsonManager.insert(Proposal.class, jsonManager.toJsonNode(proposal));
     }
 
-    public Proposal get(String uuid) {
+    public void reject(String proposalUuid) {
+        jsonManager.update(Proposal.class, p -> {
+            return p.getProposalUuid().equals(proposalUuid);
+        }, u -> {
+            u.setStatus(ProposalStatus.rejected);
+            return u;
+        });
+    }
+
+    private Proposal setProposalStatusToAccepted(String proposalUuid) {
+        final ArrayList<Proposal> result = jsonManager.update(Proposal.class, p -> {
+            return p.getProposalUuid().equals(proposalUuid);
+        }, u -> {
+            u.setStatus(ProposalStatus.accepted);
+            return u;
+        });
+
+        if (result.isEmpty()) {
+            return null;
+        }
+
+        return result.getFirst();
+    }
+
+    private void setProposalsStatusToSameOfferAsRejected(Proposal proposal) {
+        jsonManager.update(Proposal.class, p -> {
+            final String offerUuid = p.getOfferUuid();
+
+            return offerUuid.equals(proposal.getOfferUuid()) && !p.getProposalUuid().equals(proposal.getProposalUuid());
+        }, u -> {
+            u.setStatus(ProposalStatus.rejected);
+            return u;
+        });
+    }
+
+    private void updateOfferStatusToSoldByProposal(Proposal proposal) {
+        offerJsonManager.update(Offer.class, o -> {
+            return o.getUuid().equals(proposal.getOfferUuid());
+        }, u -> {
+            u.setStatus(OfferStatus.sold);
+            u.setAcceptedValue(proposal.getProposalValue());
+            return u;
+        });
+    }
+
+    public void accept(String proposalUuid) {
+        // Define a proposta com o status de aceita
+        final Proposal proposal = setProposalStatusToAccepted(proposalUuid);
+        if (proposal == null) {
+            return;
+        }
+        // Fecha as outras propostas para a mesma oferta
+        setProposalsStatusToSameOfferAsRejected(proposal);
+        // Atualiza o status da oferta para vendida
+        updateOfferStatusToSoldByProposal(proposal);
+    }
+
+    public Proposal getByUuid(String uuid) {
         final ArrayList<Proposal> result = jsonManager.select(Proposal.class, p -> {
             return p.getProposalUuid().equals(uuid);
         });
@@ -28,23 +90,23 @@ public class ProposalService {
         if (result.isEmpty()) {
             return null;
         }
-        
+
         return result.getFirst();
     }
 
-    public ArrayList<Proposal> getAllFromBuyer(String buyerUuid) {
+    public ArrayList<Proposal> getAllByBuyerUuid(String buyerUuid) {
         return jsonManager.select(Proposal.class, p -> {
             return p.getBuyerUuid().equals(buyerUuid);
         });
     }
 
-    public ArrayList<Proposal> getAllFromSupplier(String supplierUuid) {
+    public ArrayList<Proposal> getAllBySupplierUuid(String supplierUuid) {
         return jsonManager.select(Proposal.class, p -> {
             return p.getSupplierUuid().equals(supplierUuid);
         });
     }
 
-    public ArrayList<Proposal> getAllFromOffer(String offerUuid) {
+    public ArrayList<Proposal> getAllByOfferUuid(String offerUuid) {
         return jsonManager.select(Proposal.class, p -> {
             return p.getOfferUuid().equals(offerUuid);
         });
