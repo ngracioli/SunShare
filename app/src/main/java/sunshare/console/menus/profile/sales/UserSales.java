1c0fcd4408a4a05208951f42f22a494b36c98250
ngracioli <nicolas.gracioli@gmail.com>
2025-05-23 22:13:37 -0300

    feat: create UserSales class to display sales history for suppliers

---
+++ app/src/main/java/sunshare/console/menus/profile/sales/UserSales.java
@@ -0,0 +1,51 @@
+package sunshare.console.menus.profile.sales;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import sunshare.entities.user.User;
+import sunshare.services.OfferService;
+import sunshare.services.ProposalService;
+import sunshare.services.UserService;
+import sunshare.entities.offer.Offer;
+import sunshare.entities.proposal.Proposal;
+import sunshare.entities.proposal.ProposalStatus;
+import sunshare.console.menus.utils.ConsoleUtils;
+
+public class UserSales {
+
+    public UserSales(Scanner scanner, User user) {
+        ProposalService proposalService = new ProposalService();
+        OfferService offerService = new OfferService();
+        UserService userService = new UserService();
+        ArrayList<Proposal> sales = proposalService.getAllBySupplierUuid(user.getUuid());
+        sales.removeIf(sale -> sale.getStatus() != ProposalStatus.accepted);
+
+        ConsoleUtils.clearConsole();
+        ConsoleUtils.printTitle("-- Histórico de Vendas --");
+        ConsoleUtils.printError("Pressione ENTER para voltar\n");
+        if (sales.isEmpty()) {
+            ConsoleUtils.printError("Nenhuma venda encontrada.");
+        } else {
+            for (int i = 0; i < sales.size(); i++) {
+
+                Proposal sale = sales.get(i);
+                Offer offer = offerService.getByUuid(sale.getOfferUuid());
+                User buyer = userService.getByUuid(sale.getBuyerUuid());
+
+                ConsoleUtils.printSuccess(String.format("Venda %d:", i + 1));
+                String saleInfo = String.format(
+                        "Comprador: %s\n" +
+                                "Produto: %s %s\n" +
+                                "Valor final: R$ %.2f",
+                        buyer.getName(),
+                        offer.getEnergy().getQuantity(),
+                        offer.getEnergy().getSymbol(),
+                        sale.getProposalValue());
+                ConsoleUtils.printOption(saleInfo);
+            }
+        }
+        scanner.nextLine();
+        return;
+    }
+}
