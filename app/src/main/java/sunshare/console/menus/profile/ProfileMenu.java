a402d5f4b3b00098c3bf52f3f8cd2cba948a7a08
ngracioli <nicolas.gracioli@gmail.com>
2025-05-24 19:18:19 -0300

    feat: add notification count display to ProfileMenu

--- app/src/main/java/sunshare/console/menus/profile/ProfileMenu.java
+++ app/src/main/java/sunshare/console/menus/profile/ProfileMenu.java
@@ -2,8 +2,11 @@ package sunshare.console.menus.profile;
 
 import java.util.Scanner;
 
+import javax.management.Notification;
+
 import sunshare.console.menus.offer.OfferMenu;
 import sunshare.console.menus.profile.infos.InfoMenu;
+import sunshare.console.menus.profile.notifications.UserNotification;
 import sunshare.console.menus.profile.purchases.UserPurchases;
 import sunshare.console.menus.profile.sales.UserSales;
 import sunshare.console.menus.utils.ConsoleUtils;
@@ -11,6 +14,7 @@ import sunshare.console.menus.utils.InputUtils;
 import sunshare.console.menus.profile.proposals.ProposalMenu;
 import sunshare.console.menus.profile.proposals.user.UserProposals;
 import sunshare.entities.user.User;
+import sunshare.services.NotificationService;
 
 public class ProfileMenu {
     private boolean exit = false;
@@ -22,18 +26,26 @@ public class ProfileMenu {
     }
 
     public void showMenu(Scanner scanner, User user) {
+
+        NotificationService notificationService = new NotificationService();
+        final int notificationCount = notificationService.getQuantityOfNotifications(user.getUuid());
+
         ConsoleUtils.clearConsole();
         ConsoleUtils.printTitle("Perfil");
         ConsoleUtils.printExit("0. Voltar");
         ConsoleUtils.printOption("1. Minha informações");
-        ConsoleUtils.printOption("2. Notificações");
+        if (notificationCount > 0) {
+            ConsoleUtils.printApproved("2. Notificações (" + notificationCount + ")");
+        } else {
+            ConsoleUtils.printOption("2. Notificações (0)");
+        }
         ConsoleUtils.printOption("3. Histórico de compras");
         ConsoleUtils.printOption("4. Propostas");
         if (user.isSupplier()) {
             ConsoleUtils.printOption("5. Vendas efetuadas");
         }
         int option = InputUtils.readIntOption(scanner, "Escolha uma opção: ", "Digite uma opção válida.", 0, 1, 2, 3,
-                4,5);
+                4, 5);
         switch (option) {
             case 0:
                 exit = true;
@@ -42,7 +54,7 @@ public class ProfileMenu {
                 new InfoMenu(scanner, user);
                 break;
             case 2:
-                //notificações menu
+                new UserNotification(scanner, user);
                 break;
             case 3:
                 new UserPurchases(scanner, user);
