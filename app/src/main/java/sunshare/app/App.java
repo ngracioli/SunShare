a685dcb2ddd4d8cb494876da7ff3b140f496f593
ngracioli <nicolas.gracioli@gmail.com>
2025-05-24 19:20:12 -0300

    feat: add JsonInitializer to App main method for database setup

--- app/src/main/java/sunshare/app/App.java
+++ app/src/main/java/sunshare/app/App.java
@@ -1,12 +1,15 @@
 package sunshare.app;
 
 import sunshare.console.menus.entry.EntryMenu;
+import sunshare.json.manager.JsonInitializer;
 import sunshare.services.AuthService;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+        JsonInitializer.initialize();
+
         final AuthService authService = new AuthService();
         final Scanner scanner = new Scanner(System.in);
 
