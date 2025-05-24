adaadeeb90f0815a6af791ee0a91246f5686a60f
ngracioli <nicolas.gracioli@gmail.com>
2025-05-23 22:14:32 -0300

    feat: add notifications menu and UserSales to ProfileMenu

--- app/src/main/java/sunshare/console/menus/profile/ProfileMenu.java
+++ app/src/main/java/sunshare/console/menus/profile/ProfileMenu.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 import sunshare.console.menus.offer.OfferMenu;
 import sunshare.console.menus.profile.infos.InfoMenu;
+import sunshare.console.menus.profile.purchases.UserPurchases;
+import sunshare.console.menus.profile.sales.UserSales;
 import sunshare.console.menus.utils.ConsoleUtils;
 import sunshare.console.menus.utils.InputUtils;
 import sunshare.console.menus.profile.proposals.ProposalMenu;
@@ -24,13 +26,14 @@ public class ProfileMenu {
         ConsoleUtils.printTitle("Perfil");
         ConsoleUtils.printExit("0. Voltar");
         ConsoleUtils.printOption("1. Minha informações");
-        ConsoleUtils.printOption("2. Histórico de compras");
-        ConsoleUtils.printOption("3. Propostas");
+        ConsoleUtils.printOption("2. Notificações");
+        ConsoleUtils.printOption("3. Histórico de compras");
+        ConsoleUtils.printOption("4. Propostas");
         if (user.isSupplier()) {
-            ConsoleUtils.printOption("4. Vendas efetuadas");
+            ConsoleUtils.printOption("5. Vendas efetuadas");
         }
         int option = InputUtils.readIntOption(scanner, "Escolha uma opção: ", "Digite uma opção válida.", 0, 1, 2, 3,
-                4);
+                4,5);
         switch (option) {
             case 0:
                 exit = true;
@@ -39,19 +42,21 @@ public class ProfileMenu {
                 new InfoMenu(scanner, user);
                 break;
             case 2:
-                new OfferMenu(scanner, user);
+                //notificações menu
                 break;
             case 3:
+                new UserPurchases(scanner, user);
+                break;
+            case 4:
                 if (!user.isSupplier()) {
                     new UserProposals(scanner, user);
                     return;
                 }
                 new ProposalMenu(scanner, user);
                 break;
-            case 4:
+            case 5:
                 if (user.isSupplier()) {
-                    ConsoleUtils.printOption("VC É VENDEDOR");
-                    ConsoleUtils.timerConsole(2000);
+                    new UserSales(scanner, user);
                     break;
                 }
             default:
