b35671855980e383a8ff388eb91120cc1ac3f395
ngracioli <nicolas.gracioli@gmail.com>
2025-05-24 19:16:20 -0300

    refactor: change BaseService constructor parameter from String to JsonFiles

--- app/src/main/java/sunshare/services/BaseService.java
+++ app/src/main/java/sunshare/services/BaseService.java
@@ -2,13 +2,14 @@ package sunshare.services;
 
 import java.util.UUID;
 
+import sunshare.json.manager.JsonFiles;
 import sunshare.json.manager.JsonManager;
 
 public class BaseService {
     protected final JsonManager jsonManager;
 
-    public BaseService(String file) {
-        this.jsonManager = new JsonManager(file);
+    public BaseService(JsonFiles file) {
+        this.jsonManager = new JsonManager(file.getFile());
     }
 
     String generateUuid() {
