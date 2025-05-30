8d032ca93e50238551eebc8d17105df83060239a
ngracioli <nicolas.gracioli@gmail.com>
2025-05-29 21:45:30 -0300

    feat: show open offers and update user info on profile entry

--- app/src/main/java/sunshare/console/menus/profile/infos/InfoMenu.java
+++ app/src/main/java/sunshare/console/menus/profile/infos/InfoMenu.java
@@ -5,20 +5,40 @@ import java.util.Scanner;
 import sunshare.console.menus.utils.ConsoleUtils;
 import sunshare.entities.address.Address;
 import sunshare.entities.document.Document;
+import sunshare.entities.offer.OfferStatus;
 import sunshare.entities.user.User;
+import sunshare.services.OfferService;
+import sunshare.services.UserService;
 
 public class InfoMenu {
+    int quantityOfOffers = 0;
 
-    public InfoMenu(Scanner scanner, User user) {
+    public InfoMenu(Scanner scanner, User userOld) {
+        UserService userService = new UserService();
+        User user = userService.getByUuid(userOld.getUuid());
         final Address address = user.getAddress();
         final Document document = user.getDocument();
+
+        if (user.isSupplier()) {
+            OfferService offerService = new OfferService();
+            quantityOfOffers = 0;
+            offerService.getAllFromUser(user.getUuid()).forEach(offer -> {
+                if (offer.getStatus() == OfferStatus.open) {
+                    quantityOfOffers++;
+                }
+            });
+        }
+
         ConsoleUtils.clearConsole();
-        ConsoleUtils.printTitle("Minhas informações");
+        ConsoleUtils.printTitle("-- Minhas informações --");
         ConsoleUtils.printSuccess("Nome: " + user.getName());
         ConsoleUtils.printSuccess("Email: " + user.getEmail());
         ConsoleUtils.printSuccess("Tipo: " + (user.isSupplier() ? "Fornecedor" : "Consumidor"));
-        ConsoleUtils.printSuccess("Compras:" + user.getTotalPurchases());
-        ConsoleUtils.printSuccess("Vendas: " + user.getTotalSales());
+        ConsoleUtils.printSuccess("Compras realizadas: " + user.getTotalPurchases());
+        if (user.isSupplier()) {
+            ConsoleUtils.printSuccess("Ofertas abertos: " + quantityOfOffers);
+            ConsoleUtils.printSuccess("Vendas efetuadas: " + user.getTotalSales());
+        }
         ConsoleUtils.printSuccess("Endereço: " + address.getStreet() + ", " + address.getNeighborhood() + ", "
                 + address.getCity() + ", " + address.getState() + ", " + address.getCep());
         ConsoleUtils.printSuccess("Tipo de documento: " + document.getDocumentType().type);
